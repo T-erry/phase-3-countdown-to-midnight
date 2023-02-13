3eb8fb51ee1bdc6a3f1ab4953fe432fd76f019b3
T-erry <terrywachuka10@gmail.com>
2023-02-13 11:03:45 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
-
-def countdown
+require 'pry'
+def countdown int = 10
+    while int >=  1
+      puts  "#{int} SECOND(S)!"
+         int -= 1
+        #  binding.pry
+    end
+    "HAPPY NEW YEAR!"      
+end
+def countdown_with_sleep int
+    sleep(int)
 end
